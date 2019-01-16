bf7cb969f7e89a94fdd37301320b655a821bd522
Marcy Silverman <marcysilve225@gmail.com>
2019-01-15 17:32:53 -0800

    rick and morty api works

--- test/rickandMortyServertest.js
+++ test/rickandMortyServertest.js
@@ -1,11 +1,11 @@
-// const { getCharacter } = require('../lib/rickAndMortyApi');
+// const { getCharacter } = require('../lib/rickAndMortyServer');
 
 // describe('Rick and Morty service', () => {
 //   it('gets a character by id', () => {
-//     return getCharacter(1) 
+//     return getCharacter(2) 
 //       .then(character => {
 //         expect(character).toEqual({
-//           name: 'Rich Sanchez',
+//           name: 'Rick Sanchez',
 //           status: 'Alive',
 //           species: 'Human'
 //         });
